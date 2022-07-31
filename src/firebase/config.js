120bc152167c123b7a17d3a502f958fc0bd99f02
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-31 14:47:26 -0400

    Install firebase and created config file to connect to firebase database

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs7L8BA9PMsd0bHRStZkk1vDl6CIgj1Mg",
+  authDomain: "thedojo-app.firebaseapp.com",
+  projectId: "thedojo-app",
+  storageBucket: "thedojo-app.appspot.com",
+  messagingSenderId: "1042824801419",
+  appId: "1:1042824801419:web:7492fec4b0dd3c9299388e"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
