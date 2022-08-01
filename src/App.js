d110b219b9c9fab28fddc97fce42a055a879a8e4
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-01 14:49:28 -0400

    Import all pages and setup all routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// styles
 import './App.css'
+
+// pages
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Create from './pages/create/Create'
+import Project from './pages/project/Project'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/projects/:id'>
+              <Project />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
