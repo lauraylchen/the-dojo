a2d695af7a0b6b8fbd728daf1ead222e501a8601
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-01 16:44:12 -0400

    Import Sidebar component to add it on the left side of the App

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ import Project from './pages/project/Project'
 
 // components
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className="container">
           <Navbar />
           <Switch>
