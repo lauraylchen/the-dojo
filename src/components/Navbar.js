e0e14102d8c0308e5b244e61e3f54cb26bca6313
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-01 15:32:32 -0400

    Navbar content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,24 @@
-// styles
+import { Link } from 'react-router-dom'
+
+// styles & images
 import './Navbar.css'
+import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <ul>
+        <li className="logo">
+          <img src={Temple} alt='dojo logo' />
+          <span>The Dojo</span>
+        </li>
+
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/signup'>Signup</Link></li>
+        <li>
+          <button className="btn">Logout</button>
+        </li>
+      </ul>
+    </div>
   )
 }
