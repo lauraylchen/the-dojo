98caecb7ab7fa2bbecf01c359685e4031ad1aa60
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-01 16:26:13 -0400

    Add template of Sidebar with missing user image and username

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,35 @@
-// styles
+import { NavLink } from 'react-router-dom'
+
+// styles & images
 import './Sidebar.css'
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className="sidebar-content">
+        <div className="user">
+          {/* avatar and username here later */}
+          <p>Hey user</p>
+        </div>
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink to='/'>
+                <img src={DashboardIcon} alt="dashboard icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/create'>
+                <img src={AddIcon} alt="add project icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   )
 }
