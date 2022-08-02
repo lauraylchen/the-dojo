f6969626ecfabf106a0b8fb8f2f7218c28db25ca
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-02 12:24:43 -0400

    Signup user on submit

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 
 // styles
 import './Signup.css'
@@ -9,6 +10,12 @@ export default function Signup() {
   const [displayName, setDisplayName] = useState('')
   const [thumbnail, setThumbnail] = useState(null)
   const [thumbnailError, setThumbnailError] = useState(null)
+  const { signup, isPending, error } = useSignup()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    signup(email, password, displayName, thumbnail)
+  }
 
   const handleFileChange = (e) => {
     setThumbnail(null)
@@ -34,7 +41,7 @@ export default function Signup() {
   }
 
   return (
-    <form className='auth-form'>
+    <form className='auth-form' onSubmit={handleSubmit}>
       <h2>Sign up</h2>
       <label>
         <span>Email:</span>
@@ -72,7 +79,9 @@ export default function Signup() {
         />
         {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
-      <button className="btn">Sign up</button>
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && <button className="btn" disabled>Loading</button>}
+      {error && <div className='error'>{error}</div>}
     </form>
   )
 }
