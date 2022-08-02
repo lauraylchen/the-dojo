2cf2cf7b101028274aca4787de1c5a71df043e7e
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-02 11:34:53 -0400

    Thumbnail setup error checks

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,6 +8,30 @@ export default function Signup() {
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
   const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailError, setThumbnailError] = useState(null)
+
+  const handleFileChange = (e) => {
+    setThumbnail(null)
+    let selected = e.target.files[0]
+
+    if (!selected) {
+      setThumbnailError('Please select a file')
+      return
+    }
+
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image')
+      return
+    }
+
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb')
+      return
+    }
+
+    setThumbnailError(null)
+    setThumbnail(selected)
+  }
 
   return (
     <form className='auth-form'>
@@ -44,7 +68,9 @@ export default function Signup() {
         <input
           type="file"
           required
+          onChange={handleFileChange}
         />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
       <button className="btn">Sign up</button>
     </form>
