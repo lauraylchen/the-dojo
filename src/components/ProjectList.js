61a22021c6211339de0d4be2f9ae852f6b18f35d
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-02 23:17:32 -0400

    Added CSS styles to projectlist

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,7 +8,7 @@ import Avatar from './Avatar'
 
 export default function ProjectList({ projects }) {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
         <Link to={`/projects/${project.id}`} key={project.id}>
