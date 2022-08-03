4f4cb173101a39e32015d8bbd79b93a3612ca3b6
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-03 15:48:17 -0400

    Add projectFilter component on top of dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import './Dashboard.css'
 
 // components
 import ProjectList from '../../components/ProjectList'
+import ProjectFilter from './ProjectFilter'
 
 export default function Dashboard() {
   const { documents, error } = useCollection('projects')
@@ -13,6 +14,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
