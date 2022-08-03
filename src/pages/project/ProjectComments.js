7876cfa49bcf3e639ee6d690477cd113638ee535
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-03 13:33:02 -0400

    Output all the comments of the project

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,6 +3,9 @@ import { timestamp } from '../../firebase/config'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useFirestore } from '../../hooks/useFirestore'
 
+// components
+import Avatar from '../../components/Avatar'
+
 export default function ProjectComments({ project }) {
   const { updateDocument, response } = useFirestore('projects')
   const [newComment, setNewComment] = useState('')
@@ -30,6 +33,23 @@ export default function ProjectComments({ project }) {
   return (
     <div className='project-comments'>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL} />
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
