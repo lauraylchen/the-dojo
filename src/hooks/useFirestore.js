d29842238f6abcdf74ef1f84328e7a5414392ab8
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-03 13:18:27 -0400

    Created a update action to update the comments array for project

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -16,6 +16,8 @@ const firestoreReducer = (state, action) => {
       return { isPending: false, document: action.payload, success: true, error: null }
     case 'DELETED_DOCUMENT':
       return { isPending: false, document: null, success: true, error: null }
+    case 'UPDATED_DOCUMENT':
+      return { isPending: false, document: action.payload, success: true, error: null }
     case 'ERROR':
       return { isPending: false, document: null, success: false, error: action.payload }
     default:
@@ -51,6 +53,21 @@ export const useFirestore = (collection) => {
     }
   }
 
+  // update documents
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: 'IS_PENDING' })
+
+    try {
+      const updatedDocument = await ref.doc(id).update(updates)
+      dispatchIfNotCancelled({ type: 'UPDATED_DOCUMENT', payload: updatedDocument })
+      return updatedDocument
+    }
+    catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: 'could not delete' })
+      return null
+    }
+  }
+
   // delete a document
   const deleteDocument = async (id) => {
     dispatch({ type: 'IS_PENDING' })
@@ -68,6 +85,6 @@ export const useFirestore = (collection) => {
     return () => setIsCancelled(true)
   }, [])
 
-  return { addDocument, deleteDocument, response }
+  return { addDocument, deleteDocument, updateDocument, response }
 
 }
