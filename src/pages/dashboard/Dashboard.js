0f60d14357abb87dbce39d76dea407f0a6b33adb
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-04 11:31:28 -0400

    filter the documents receive from firestore before listing them out

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import { useCollection } from '../../hooks/useCollection'
 import { useState } from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 // styles
 import './Dashboard.css'
@@ -11,10 +12,34 @@ import ProjectFilter from './ProjectFilter'
 export default function Dashboard() {
   const { documents, error } = useCollection('projects')
   const [currentFilter, setCurrentFilter] = useState('all')
+  const { user } = useAuthContext()
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter)
   }
+
+  const projects = documents ? documents.filter((document) => {
+    switch (currentFilter) {
+      case 'all':
+        return true
+      case 'mine':
+        let assignedToMe = false
+        document.assignedUsersList.forEach((u) => {
+          if (user.uid === u.id) {
+            assignedToMe = true
+          }
+        })
+        return assignedToMe
+      case 'development':
+      case 'design':
+      case 'sales':
+      case 'marketing':
+        console.log(document.category, currentFilter)
+        return document.category === currentFilter
+      default:
+        return true
+    }
+  }) : null
 
   return (
     <div>
@@ -23,7 +48,7 @@ export default function Dashboard() {
       {documents && (
         <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter} />
       )}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   )
 }
